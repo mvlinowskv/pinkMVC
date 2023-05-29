8ce2cbedd093969ad62c96471a460a4b642856d1
kmal <klaudia.malinowska@softwebo.pl>
2023-05-29 09:31:54 +0200

    create section build and nav

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,17 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+const btn = document.querySelector('.btn-menu')
+
+const nav = document.querySelector('nav')
+
+document.addEventListener('click', function(e) {
+    if(e.target.closest('.btn-menu')) {
+        nav.style.right = 0;
+        nav.style.transition = 'right 0.4s ease-out';
+    } else {
+        nav.style.right = '-100vw';
+        nav.style.transition = 'right 0.4s ease-out';
+    }
+})
